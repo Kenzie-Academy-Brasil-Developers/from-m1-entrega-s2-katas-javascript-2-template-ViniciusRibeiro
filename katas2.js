365582d2df6b5d4019f0ea23ae33ec36606ca9a4
vinicius <viniciusribeiro.dev@hotmail.com>
2022-10-27 14:12:28 -0300

    Entraga S5-10

--- katas2.js
+++ katas2.js
@@ -1,1 +1,27 @@
+function add(a, b) {
+  return a + b;
+}
 
+function multiply(c, b) {
+  let multiplicacao = c;
+  for (let i = 1; i < b; i++) {
+    multiplicacao = add(multiplicacao, c);
+  }
+  return multiplicacao;
+}
+
+function power(x, n) {
+  let potencia = x;
+  for (let i = 1; i < n; i++) {
+    potencia = multiply(potencia, n);
+  }
+  return potencia;
+}
+
+function factorial(v) {
+  let fatorial = 1;
+  for (let i = v; i >= 1; i--) {
+    fatorial = multiply(fatorial, i);
+  }
+  return fatorial;
+}
